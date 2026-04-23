af8597d76a13ebb22000a2e85aa726986d3eb6c2
frisk-V3 <260639938+frisk-V3@users.noreply.github.com>
2026-04-23 20:07:32 +0900

    Update window.rs

--- src/window.rs
+++ src/window.rs
@@ -1,22 +1,23 @@
 use tao::{
     event_loop::EventLoop,
-    window::{Window, WindowBuilder},
+    window::WindowBuilder,
 };
 
-use wry::webview::WebViewBuilder;
+use wry::WebViewBuilder;
 
-pub fn create_window(event_loop: &EventLoop<()>, url: &str) -> Window {
+// Windowじゃなくて WebView を返すのが正解
+pub fn create_window(event_loop: &EventLoop<()>, url: &str) -> wry::WebView {
     let window = WindowBuilder::new()
         .with_title("Frisk Browser")
         .build(event_loop)
         .expect("Failed to create window");
 
-    WebViewBuilder::new(window.clone())
+    let webview = WebViewBuilder::new(window)
         .expect("WebView init failed")
         .with_url(url)
         .expect("Invalid URL")
         .build()
         .expect("Failed to build WebView");
 
-    window
+    webview
 }
