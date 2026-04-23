a3f3aed33b0c95663fb81482483343d3d9105a60
frisk-V3 <260639938+frisk-V3@users.noreply.github.com>
2026-04-23 20:07:42 +0900

    Update browser.rs

--- src/browser.rs
+++ src/browser.rs
@@ -10,12 +10,12 @@ use crate::window::create_window;
 pub fn run() {
     let event_loop = EventLoop::new();
 
-    let mut windows: HashMap<u32, tao::window::Window> = HashMap::new();
+    // WindowじゃなくてWebView管理する
+    let mut views: HashMap<u32, wry::WebView> = HashMap::new();
     let mut counter: u32 = 0;
 
-    // 初期タブ（= ウィンドウ）
-    let win = create_window(&event_loop, "https://www.google.com");
-    windows.insert(counter, win);
+    let view = create_window(&event_loop, "https://www.google.com");
+    views.insert(counter, view);
     counter += 1;
 
     event_loop.run(move |event, _, control_flow| {
@@ -27,11 +27,9 @@ pub fn run() {
                 window_id,
                 ..
             } => {
-                // 該当ウィンドウ削除
-                windows.retain(|_, w| w.id() != window_id);
+                views.retain(|_, v| v.window().id() != window_id);
 
-                // 全部閉じたら終了
-                if windows.is_empty() {
+                if views.is_empty() {
                     *control_flow = ControlFlow::Exit;
                 }
             }
